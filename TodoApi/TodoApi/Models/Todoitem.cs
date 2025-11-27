e275d1013bda8076819249c0747e9ab0b7cd93f8
ove-spec <egu643@edu.sollentuna.se>
2025-11-27 11:16:35 +0100

    Add files via upload

---
+++ TodoApi/TodoApi/Models/Todoitem.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TodoApi.Models
+{
+    [ApiController]
+    [Route("[Todoitem]")]
+    public class Todoitem : ControllerBase
+    {
+        private static readonly string[] mat = new string[]
+        {
+            "matsedeln"
+        };
+
+        
+        
+
+    }
+}
